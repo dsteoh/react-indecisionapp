d891d80c2bfd6cc2950ad658b1b2c20f8e0a6ded
Teoh <695070@cognizant.com>
2018-10-02 09:52:10 +1000

    First commit

---
+++ public/scripts/app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+console.log("App.js is running!");
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['Item One, Item Two']
+};
+
+//JSX - JavaScript XML
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    app.options.length > 0 ? React.createElement(
+        'p',
+        null,
+        'Here are your options: ',
+        app.options
+    ) : 'No options'
+);
+
+var count = 0;
+
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
+var appRoot = document.getElementById('app');
+
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset'
+        )
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
---
+++ src/playground/es6-arrowtwo.js
@@ -0,0 +1,33 @@
+// arguments objects - no longer bond with arrow functions
+const add = (a, b) => {
+    //console.log(arguments);
+    return a + b
+};
+
+console.log(add(55,1,1001));
+
+//this keyword - no longer bond
+const user = {
+    name: 'Andrew',
+    cities: ['Melbourne', 'Ipoh', 'New York'],
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has lived in ' + city);
+    }
+};
+console.log(user.printPlacesLived())    
+
+//Challenge area
+const multiplier = {
+    //number - array of numbers
+    //multiplyBy - single number
+    //multiply - return a new array where the numbers have been multiplied
+
+    number: [2,4,5,10],
+    multiply(multiplyBy) {
+        return this.number.map((numbers) => numbers * multiplyBy);
+    }
+
+
+};
+
+console.log(multiplier.multiply(3));
