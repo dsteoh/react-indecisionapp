8a805abafd76e011bd791c10cd82a177e4f9ef4b
Teoh <desiangteoh@gmail.com>
2018-10-18 15:29:16 +1100

    Webpack import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('app.js is running!!!!');
+//import './utils.js';
+// import subtract, {square, add} from './utils.js' //{} not an object
+
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 81))
+
+//person.js
+//name export isAdult(18) - true if adult, otherwise false
+//name export canDrink(18) - if 21 and over otherwise false
+
+//import isAdult and canDrink
+//print 
+
+import isSenior, {isAdult, canDrink} from './person.js';
+console.log(isAdult(20));
+console.log(canDrink(13));
+console.log(isSenior(65));
