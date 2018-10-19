b2c6d9bc9bf4cbec5fcb93c7d0d29c9f19ae9b00
dsteoh <desiangteoh@gmail.com>
2018-10-19 16:45:34 +1100

    changed explicit return to implicit return

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-const Action = (props) => {
-    return (
-        <div>
+const Action = (props) => (
+    <div>
 
-            <button onClick ={props.handlePick}
-            disabled = {!props.hasOptions }
-            >
-            What should I do
-            </button>
-        </div>
-    );
-}
+        <button onClick ={props.handlePick}
+        disabled = {!props.hasOptions }
+        >
+        What should I do
+        </button>
+    </div>
+)
 
 export default Action
